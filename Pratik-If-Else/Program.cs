840b70d805afac8127e32707aba9c9f3f21bedbf
Hasan İncel <h_incel@hotmail.com>
2024-07-19 18:51:59 +0300

    Add project files.

---
+++ Pratik-If-Else/Program.cs
@@ -0,0 +1,27 @@
+﻿// Kullanıcıdan sayı girişi alınıyor
+Console.Write("Lütfen bir sayı giriniz: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+// 10'a eşit mi, 10'dan küçük mü, 10'dan büyük mü kontrolü
+if (sayi == 10)
+{
+    Console.WriteLine("Girdiğiniz sayı 10'a eşittir.");
+}
+else if (sayi < 10)
+{
+    Console.WriteLine("Girdiğiniz sayı 10'dan küçüktür.");
+}
+else
+{
+    Console.WriteLine("Girdiğiniz sayı 10'dan büyüktür.");
+}
+
+// Çift mi, tek mi kontrolü
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Girdiğiniz sayı çifttir.");
+}
+else
+{
+    Console.WriteLine("Girdiğiniz sayı tektir.");
+}
